6ae401884268e1508151fc842276ed947db3c617
Yuri Yaryshev <yuyaryshev@gmail.com>
2021-04-30 20:15:21 +0300

    1

---
+++ dist/types.js
@@ -0,0 +1,25 @@
+import { anyJson, boolean, object, optional, string } from "@mojotech/json-type-validation";
+export const decoderAckPacket = object({
+    ok: boolean(),
+    error: optional(string()),
+});
+export const decoderOneGetApiRequest = object({
+    dataVersionOnly: optional(string()),
+});
+export const decoderOneGetApiResponse = object({
+    ok: boolean(),
+    error: optional(string()),
+    dataVersion: string(),
+    data: optional(anyJson()),
+});
+export const decoderOneSaveApiRequest = object({
+    data: anyJson(),
+    prevDataVersion: string(),
+    newDataVersion: string(),
+});
+export const decoderOneSaveApiResponse = object({
+    ok: boolean(),
+    error: optional(string()),
+});
+//----------------------------------------------------------------------------------------------------
+//# sourceMappingURL=types.js.map
